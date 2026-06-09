d2de2cbc44fd301e3952eb32cc96305c384eddd8
LauraVargas299 <lsvargass@udistrital.edu.co>
2026-06-09 01:40:30 -0500

    si

---
+++ make_html.py
@@ -0,0 +1,6 @@
+html = open("/dev/stdin").read() if False else None
+
+# Leer el HTML del documento (lo escribo inline aquí directamente)
+# En vez de eso, voy a aplicar sed directamente sobre el archivo que crearé
+
+print("script listo")
